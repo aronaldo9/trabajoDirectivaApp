3b795ff731ab0797bd2a248b5fc6e56229c450cc
AaronPFDev <aaronpesqueirafarina@gmail.com>
2025-05-12 16:34:25 +0200

    Directiva ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  titulo = 'Registro de Usuarios';
+  mensaje = '';
+  registrado = false;
+  nombre = '';
+  apellido = '';
+
+  registrarUsuario() {
+    this.registrado = true;
+    this.mensaje = `${this.nombre} ${this.apellido} registrado con éxito`;
+  }
 }
